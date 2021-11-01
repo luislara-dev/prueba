f8b5f9916597f7a419d4c1be1f4a0409529e2960
luislara-dev <xljlax@gmail.com>
2021-11-01 13:44:20 -0500

    commit

--- scraping.py
+++ scraping.py
@@ -15,10 +15,89 @@ from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 
+ruta_excel = "C:\\Users\\alex5\\Downloads\\PADRON DE ESTUDIANTES 2021.xlsx"
+nombre_hoja = "Padron Estudiantes 2021"
+
+def SepararNombres( nombre ):
+    """
+    Autor original en código PHP: eduardoromero.
+    https://gist.github.com/eduardoromero/8495437
+    
+    Separa los nombres y los apellidos y retorna una tupla de tres
+    elementos (string) formateados para nombres con el primer caracter
+    en mayuscula. Esto es suponiendo que en la cadena los nombres y 
+    apellidos esten ordenados de la forma ideal:
+ 
+    1- nombre o nombres.
+    2- primer apellido.
+    3- segundo apellido.
+ 
+    SplitNombres( '' )
+    >>> ('Nombres', 'Primer Apellido', 'Segundo Apellido')
+    """
+ 
+    # Separar el nombre completo en espacios.
+    tokens = nombre.split(" ")
+ 
+    # Lista donde se guarda las palabras del nombre.
+    names = []
+ 
+    # Palabras de apellidos y nombres compuestos.
+    especial_tokens = ['da', 'de', 'di', 'do', 'del', 'la', 'las', 
+    'le', 'los', 'mac', 'mc', 'van', 'von', 'y', 'i', 'san', 'santa']
+ 
+    prev = ""
+    for token in tokens:
+        _token = token.lower()
+ 
+        if _token in especial_tokens:
+            prev += token + " "
+ 
+        else:
+            names.append(prev + token)
+            prev = ""
+ 
+    num_nombres = len(names)
+    nombres, apellido1, apellido2 = "", "", ""
+ 
+    # Cuando no existe nombre.
+    if num_nombres == 0:
+        nombres = ""
+ 
+    # Cuando el nombre consta de un solo elemento.
+    elif num_nombres == 1:
+        apellido1 = names[0]
+ 
+    # Cuando el nombre consta de dos elementos.
+    elif num_nombres == 2:
+        apellido1 = names[0]
+        apellido2 = names[1]
+ 
+    # Cuando el nombre consta de tres elementos.
+    elif num_nombres == 3:
+        apellido1 = names[0]
+        apellido2 = names[1]
+        nombres = names[2]
+    # Cuando el nombre consta de más de tres elementos.
+    elif num_nombres == 4:
+        apellido1 = names[0]
+        apellido2 = names[1]
+        nombres = names[2] + " " + names[3]
+    elif num_nombres == 5:
+        apellido1 = names[0]
+        apellido2 = names[1]
+        nombres = names[2] + " " + names[3] + " " + names[4]
+      # Establecemos las cadenas con el primer caracter en mayúscula.
+    nombres = nombres.title()
+    apellido1 = apellido1.title()
+    apellido2 = apellido2.title()
+ 
+    return (apellido1, apellido2, nombres)
+
 def actualizarExcel(fila,dni):
     archivo = openpyxl.load_workbook(ruta_excel)
     hoja = archivo.get_sheet_by_name(nombre_hoja)
-    hoja.cell(row=fila,column=10).value = dni
+    hoja.cell(row=fila,column=4).value = dni
     archivo.save(ruta_excel)
     
 def actualizarSheet(fila,dni):
@@ -31,7 +110,12 @@ def actualizarSheet(fila,dni):
 
 def buscarDNIenWEB(nombres, ape_p, ape_m):
     s = Service("C:\\Users\\alex5\\Downloads\\chromedriver.exe")
-    driver = webdriver.Chrome(service=s)
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_experimental_option('excludeSwitches',['enable-logging']);
+    driver = webdriver.Chrome(service=s, chrome_options=options)
     driver.get("https://eldni.com/pe/buscar-por-nombres")
     driver.find_element_by_id("nombres").send_keys(nombres)
     driver.find_element_by_id("apellido_p").send_keys(ape_p)
@@ -48,24 +132,38 @@ def buscarDNIenWEB(nombres, ape_p, ape_m):
         return resultDNI
     
 def cargarDatosDesdeExcel():
-    ruta_excel = "C:\\Users\\alex5\\Downloads\\PADRON DE ESTUDIANTES 2021.xlsx"
-    nombre_hoja = "Padron Estudiantes 2021"
+    
     dfDatos = pd.read_excel(io=ruta_excel,sheet_name=nombre_hoja)
     lista = dfDatos.to_dict('list')
 
-    nombres = lista['Nombres'] 
-    apellidos_P = lista['Apellido Paterno']
-    apellidos_M = lista['Apellido Materno']
+    id = lista["ID"]
+    nombresyapellidos = lista['Apellidos y Nombres'] 
     docIden = lista['DNI']
-
-    combo = zip(nombres,apellidos_P, apellidos_M, docIden) 
+    totalFilas = len(id)
+    contDniVacios = 0
+    for i in docIden:
+        if pd.isnull(i):
+            contDniVacios+=1
+            
+    combo = zip(id,nombresyapellidos, docIden) 
     fila = 2
-    for nombres, apellidos_P, apellidos_M, docIden in combo:
-        print("Fila " + str(fila) + " " + str(docIden))
-        if (pd.isna(docIden)):
-            dni = buscarDNIenWEB(nombres,apellidos_P,apellidos_M)
-            print(str(dni) +"\t"+ nombres," ",apellidos_P," ",apellidos_M)
-            actualizarSheet(fila,dni)
+            
+    for id,nombresyapellidos, docIden in combo:
+        if (fila > 242):
+            if (pd.isna(docIden)):
+                nombrescompleto = SepararNombres(nombresyapellidos)
+                ap = nombrescompleto[0]
+                am = nombrescompleto[1]
+                nombres = nombrescompleto[2]
+                dni = buscarDNIenWEB(nombres,ap,am)
+                if (dni == ""):
+                    print("ID " + str(id) + " " + str(docIden) +"\t"+ nombres," ",ap," ",am + " [No se encontro DNI]")
+                else :
+                    print("ID " + str(id) + " " + str(docIden) +"\t"+ nombres," ",ap," ",am + " --SE ENCONTRÓ dni --" +  dni)
+                    print("Faltan buscar " + str(contDniVacios) + " registros" )
+                    #actualizarExcel(fila,dni)
+                    #actualizarSheet(fila,dni)
+                    contDniVacios -= 1
         fila = fila + 1
     
 def cargarDatosDesdeSheet():
@@ -77,25 +175,24 @@ def cargarDatosDesdeSheet():
     nombresyapellidos = sheet.get_worksheet(0).col_values(3)
     docIden = sheet.get_worksheet(0).col_values(4)
     combo = zip(id,nombresyapellidos, docIden) 
-    
+    contDniVacios = docIden.count("")
     for id, nombresyapellidos, docIden in combo:
         if id == "ID":
             continue
         if docIden == "" :
-            lista = str(nombresyapellidos).split()
-            ap = lista[0]
-            am = lista[1]
-            nombres = []
-            cantNom = len(lista) - 2
-            while cantNom > 0:
-                nombres.append(lista[len(lista) - cantNom])
-                cantNom = cantNom - 1
-            print("ID " + str(id) + " " + str(docIden) + " " + ap + " " + am + " " + " ".join(nombres))
-            dni = buscarDNIenWEB(" ".join(nombres),ap,am)
+            nombrescompleto = SepararNombres(nombresyapellidos)
+            ap = nombrescompleto[0]
+            am = nombrescompleto[1]
+            nombres = nombrescompleto[2]
+            print("ID " + str(id) + " " + str(docIden) + " [" + ap + "] [" + am + "] [" + nombres + "]")
+            print("faltan :"+ str(contDniVacios))
+            dni = buscarDNIenWEB(nombres,ap,am)
             actualizarSheet(int(id)+1,dni)
+            contDniVacios -= 1
     
 print("Iniciando scraper")
-cargarDatosDesdeSheet()
+#cargarDatosDesdeSheet()
+cargarDatosDesdeExcel()
 
 
 
