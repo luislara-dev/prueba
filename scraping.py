731951510d9eefaf6adb6c21d2f5ffe94983841e
luislara-dev <xljlax@gmail.com>
2021-10-31 20:56:10 -0500

    scraping

--- scraping.py
+++ scraping.py
@@ -1,29 +1,33 @@
 
 import time
-from pandas.core.frame import DataFrame
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common import by
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import openpyxl
+import gspread
+import pandas as pd
+import json 
+from bs4.element import Stylesheet
+from gspread.models import Worksheet
+from pandas.core.frame import DataFrame
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+from oauth2client.service_account import ServiceAccountCredentials
 
-print("Iniciando scraper")
-ruta_excel = "C:\\Users\\alex5\\Downloads\\PADRON DE ESTUDIANTES 2021.xlsx"
-nombre_hoja = "Padron Estudiantes 2021"
-dfDatos = pd.read_excel(io=ruta_excel,sheet_name=nombre_hoja)
-lista = dfDatos.to_dict('list')
-
-nombres = lista['Nombres'] 
-apellidos_P = lista['Apellido Paterno']
-apellidos_M = lista['Apellido Materno']
-docIden = lista['DNI']
-
-combo = zip(nombres,apellidos_P, apellidos_M, docIden) 
+def actualizarExcel(fila,dni):
+    archivo = openpyxl.load_workbook(ruta_excel)
+    hoja = archivo.get_sheet_by_name(nombre_hoja)
+    hoja.cell(row=fila,column=10).value = dni
+    archivo.save(ruta_excel)
+    
+def actualizarSheet(fila,dni):
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\alex5\\Downloads\\proyectopruebas-330717-919f78f2d4b3.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('PADRON DE ESTUDIANTES 2021  - UNFV PSICOLOGIA')
+    sheet_instance = sheet.get_worksheet(0)
+    sheet_instance.update_cell(fila, 5, dni)
 
 def buscarDNIenWEB(nombres, ape_p, ape_m):
     s = Service("C:\\Users\\alex5\\Downloads\\chromedriver.exe")
@@ -42,20 +46,56 @@ def buscarDNIenWEB(nombres, ape_p, ape_m):
     except : 
         resultDNI = ""
         return resultDNI
+    
+def cargarDatosDesdeExcel():
+    ruta_excel = "C:\\Users\\alex5\\Downloads\\PADRON DE ESTUDIANTES 2021.xlsx"
+    nombre_hoja = "Padron Estudiantes 2021"
+    dfDatos = pd.read_excel(io=ruta_excel,sheet_name=nombre_hoja)
+    lista = dfDatos.to_dict('list')
 
-fila = 2
-for nombres, apellidos_P, apellidos_M, docIden in combo:
-    print("Fila " + str(fila) + " " + str(docIden))
-    if (pd.isna(docIden)):
-        dni = buscarDNIenWEB(nombres,apellidos_P,apellidos_M)
-        print(str(dni) +"\t"+ nombres," ",apellidos_P," ",apellidos_M)
-        archivo = openpyxl.load_workbook(ruta_excel)
-        hoja = archivo.get_sheet_by_name(nombre_hoja)
-        hoja.cell(row=fila,column=10).value = dni
-        archivo.save(ruta_excel)
-    fila = fila + 1
+    nombres = lista['Nombres'] 
+    apellidos_P = lista['Apellido Paterno']
+    apellidos_M = lista['Apellido Materno']
+    docIden = lista['DNI']
 
-#####
+    combo = zip(nombres,apellidos_P, apellidos_M, docIden) 
+    fila = 2
+    for nombres, apellidos_P, apellidos_M, docIden in combo:
+        print("Fila " + str(fila) + " " + str(docIden))
+        if (pd.isna(docIden)):
+            dni = buscarDNIenWEB(nombres,apellidos_P,apellidos_M)
+            print(str(dni) +"\t"+ nombres," ",apellidos_P," ",apellidos_M)
+            actualizarSheet(fila,dni)
+        fila = fila + 1
+    
+def cargarDatosDesdeSheet():
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\alex5\\Downloads\\proyectopruebas-330717-919f78f2d4b3.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('PADRON DE ESTUDIANTES 2021  - UNFV PSICOLOGIA')
+    id = sheet.get_worksheet(0).col_values(1)
+    nombresyapellidos = sheet.get_worksheet(0).col_values(3)
+    docIden = sheet.get_worksheet(0).col_values(4)
+    combo = zip(id,nombresyapellidos, docIden) 
+    
+    for id, nombresyapellidos, docIden in combo:
+        if id == "ID":
+            continue
+        if docIden == "" :
+            lista = str(nombresyapellidos).split()
+            ap = lista[0]
+            am = lista[1]
+            nombres = []
+            cantNom = len(lista) - 2
+            while cantNom > 0:
+                nombres.append(lista[len(lista) - cantNom])
+                cantNom = cantNom - 1
+            print("ID " + str(id) + " " + str(docIden) + " " + ap + " " + am + " " + " ".join(nombres))
+            dni = buscarDNIenWEB(" ".join(nombres),ap,am)
+            actualizarSheet(int(id)+1,dni)
+    
+print("Iniciando scraper")
+cargarDatosDesdeSheet()
 
 
 
