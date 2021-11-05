5b6bf3cd916345c3b2bb485cdcf7e0e8a4119567
luislara-dev <xljlax@gmail.com>
2021-11-04 19:12:39 -0500

    commit

--- scraping.py
+++ scraping.py
@@ -1,12 +1,12 @@
 
 import time
-import requests
 import re
 import pandas as pd
 import openpyxl
 import gspread
 import pandas as pd
 import json 
+from SepararNombres import SepararNombres
 from bs4.element import Stylesheet
 from gspread.models import Worksheet
 from pandas.core.frame import DataFrame
@@ -23,83 +23,6 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\alex5\\Downloads\\proyectopruebas-330717-919f78f2d4b3.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open('PADRON DE ESTUDIANTES 2021  - UNFV PSICOLOGIA')
-###
-
-def SepararNombres( nombre ):
-    """
-    Autor original en código PHP: eduardoromero.
-    https://gist.github.com/eduardoromero/8495437
-    
-    Separa los nombres y los apellidos y retorna una tupla de tres
-    elementos (string) formateados para nombres con el primer caracter
-    en mayuscula. Esto es suponiendo que en la cadena los nombres y 
-    apellidos esten ordenados de la forma ideal:
- 
-    1- nombre o nombres.
-    2- primer apellido.
-    3- segundo apellido.
- 
-    SplitNombres( '' )
-    >>> ('Nombres', 'Primer Apellido', 'Segundo Apellido')
-    """
- 
-    # Separar el nombre completo en espacios.
-    tokens = nombre.split(" ")
- 
-    # Lista donde se guarda las palabras del nombre.
-    names = []
- 
-    # Palabras de apellidos y nombres compuestos.
-    especial_tokens = ['da', 'de', 'di', 'do', 'del', 'la', 'las', 
-    'le', 'los', 'mac', 'mc', 'van', 'von', 'y', 'i', 'san', 'santa']
- 
-    prev = ""
-    for token in tokens:
-        _token = token.lower()
- 
-        if _token in especial_tokens:
-            prev += token + " "
- 
-        else:
-            names.append(prev + token)
-            prev = ""
- 
-    num_nombres = len(names)
-    nombres, apellido1, apellido2 = "", "", ""
- 
-    # Cuando no existe nombre.
-    if num_nombres == 0:
-        nombres = ""
- 
-    # Cuando el nombre consta de un solo elemento.
-    elif num_nombres == 1:
-        apellido1 = names[0]
- 
-    # Cuando el nombre consta de dos elementos.
-    elif num_nombres == 2:
-        apellido1 = names[0]
-        apellido2 = names[1]
- 
-    # Cuando el nombre consta de tres elementos.
-    elif num_nombres == 3:
-        apellido1 = names[0]
-        apellido2 = names[1]
-        nombres = names[2]
-    # Cuando el nombre consta de más de tres elementos.
-    elif num_nombres == 4:
-        apellido1 = names[0]
-        apellido2 = names[1]
-        nombres = names[2] + " " + names[3]
-    elif num_nombres == 5:
-        apellido1 = names[0]
-        apellido2 = names[1]
-        nombres = names[2] + " " + names[3] + " " + names[4]
-      # Establecemos las cadenas con el primer caracter en mayúscula.
-    nombres = nombres.title()
-    apellido1 = apellido1.title()
-    apellido2 = apellido2.title()
- 
-    return (apellido1, apellido2, nombres)
 
 def actualizarExcel(fila,dni):
     archivo = openpyxl.load_workbook(ruta_excel)
@@ -108,9 +31,9 @@ def actualizarExcel(fila,dni):
     archivo.save(ruta_excel)
     
 def actualizarSheet(fila,dni):
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\alex5\\Downloads\\proyectopruebas-330717-919f78f2d4b3.json', scope)
-    client = gspread.authorize(creds)
+    # scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    # creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\alex5\\Downloads\\proyectopruebas-330717-919f78f2d4b3.json', scope)
+    # client = gspread.authorize(creds)
     sheet = client.open('PADRON DE ESTUDIANTES 2021  - UNFV PSICOLOGIA')
     sheet_instance = sheet.get_worksheet(0)
     sheet_instance.update_cell(fila, 4, dni)
